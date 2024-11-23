ac288b89a25aa5d4edb6d68456e3e61cd8971bd4
Ryan McKeever <rkfmckee@gmail.com>
2024-11-23 11:31:11 +0000

    Add item list table

--- src/helpers/Types.tsx
+++ src/helpers/Types.tsx
@@ -5,3 +5,14 @@ export type Module = {
 	details: string;
 	image: string;
 };
+
+export type Item = {
+	id: number;
+	name: string;
+	type: string;
+	subtype: string;
+	rarity: string;
+	requiresAttunement: boolean;
+	description: string;
+	moduleId: number;
+};
--- src/pages/ItemList.tsx
+++ src/pages/ItemList.tsx
@@ -1,9 +1,56 @@
-import { Typography } from "@mui/material";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Typography from "@mui/material/Typography";
+import Items from "../assets/items.json";
+import { Item } from "../helpers/Types";
 
 export default function ItemList() {
 	return (
 		<>
 			<Typography variant="h1">Items</Typography>
+			<TableContainer component={Paper}>
+				<Table sx={{ minWidth: 650 }} aria-label="item-list">
+					<TableHead>
+						<TableRow>
+							<TableCell>Name</TableCell>
+							<TableCell>Type</TableCell>
+							<TableCell>Rarity</TableCell>
+							<TableCell>Attunement</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{Items.map((item) => (
+							<TableRow
+								key={`item-${item.id}`}
+								sx={{
+									"&:last-child td, &:last-child th": {
+										border: 0,
+									},
+								}}
+							>
+								<TableCell component="th" scope="row">
+									{item.name}
+								</TableCell>
+								<TableCell>{getType(item)}</TableCell>
+								<TableCell>{item.rarity}</TableCell>
+								<TableCell>
+									{item.requiresAttunement ? "Yes" : "No"}
+								</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</TableContainer>
 		</>
 	);
+}
+
+function getType(item: Item) {
+	if (!item.subtype) return item.type;
+	return `${item.type} (${item.subtype})`;
 }
